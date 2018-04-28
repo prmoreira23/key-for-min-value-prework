cff622b331e66738607bf2150ae9f9d46af3bec3
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-28 16:09:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-    min = 0
+    min = 42356
     value = nil
     name_hash.each do |name, age|
         if age < min
