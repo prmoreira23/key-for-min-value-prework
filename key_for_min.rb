7b59ddfb9d29a18ddb9bc9e0513b307c79fec6b3
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-28 16:08:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,10 @@ def key_for_min_value(name_hash)
     min = 0
     value = nil
     name_hash.each do |name, age|
-        
+        if age < min
+            min = age
+            value = name
+        end
     end
     value
 end
